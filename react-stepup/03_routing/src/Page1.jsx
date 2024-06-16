66d5d6b989844a3a389188a179eb15e0c2271d68
Hiroki Hayashi <yuriyuri.9129@gmail.com>
2024-06-16 17:48:54 +0900

    URL遷移時に、stateで変数を受け渡す

--- react-stepup/03_routing/src/Page1.jsx
+++ react-stepup/03_routing/src/Page1.jsx
@@ -2,10 +2,11 @@ import {Link} from "react-router-dom";
 
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
   return (
     <div>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detailA">DetailA</Link><br/>
+      <Link to={{pathname: "/page1/detailA", state: arr}}>DetailA</Link><br/>
       <Link to="/page1/detailB">DetailB</Link><br/>
     </div>
   );
--- react-stepup/03_routing/src/Page1DetailA.jsx
+++ react-stepup/03_routing/src/Page1DetailA.jsx
@@ -1,6 +1,8 @@
-import {Link} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const Page1DetailA = () => {
+  const { state} = useLocation();
+  console.log(state);
   return (
     <div>
       <h1>Page1DetailAページです</h1>
