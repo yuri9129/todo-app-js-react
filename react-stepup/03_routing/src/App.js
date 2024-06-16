b13ade4c7aea9fe632868d71481a5b432600656e
Hiroki Hayashi <yuriyuri.9129@gmail.com>
2024-06-16 15:28:27 +0900

    react-routerの基本

---
+++ react-stepup/03_routing/src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+
+import './App.css';
+import {Home} from "./Home";
+import {Page1} from "./Page1";
+import {Page2} from "./Page2";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link><br/>
+        <Link to="/page1">Page1</Link><br/>
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/page1">
+          <Page1/>
+        </Route>
+        <Route path="/page2">
+          <Page2/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-stepup/03_routing/src/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Homeページです</h1>
+    </div>
+  );
+};
+
