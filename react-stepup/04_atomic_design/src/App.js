6977f485f3e75687d8cbffb3bf98617f099ab819
Hiroki Hayashi <yuriyuri.9129@gmail.com>
2024-06-19 11:43:39 +0900

    axiosを用いたAPIサンプル

--- react-stepup/04_atomic_design/src/App.js
+++ react-stepup/04_atomic_design/src/App.js
@@ -3,17 +3,38 @@ import './App.css';
 import {Router} from "./router/Router";
 import {UserProvider} from "./providers/UserProvider";
 import {RecoilRoot} from "recoil";
+import axios from "axios";
 
 
 
 
 function App() {
+  const onClickUsers  = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then( (res) => {
+      console.log(res.data);
+    }).catch( (err) => {
+      console.log(err);
+    });
+  }
+
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1").then( (res) => {
+      console.log(res.data);
+    }).catch( (err) => {
+      console.log(err);
+    });
+  }
+
   return (
-    <RecoilRoot>
-      <UserProvider>
-        <Router/>
-      </UserProvider>
-    </RecoilRoot>
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+    // <RecoilRoot>
+    //   <UserProvider>
+    //     <Router/>
+    //   </UserProvider>
+    // </RecoilRoot>
 
   );
 }
