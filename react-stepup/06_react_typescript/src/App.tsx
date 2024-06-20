c62236e58817c36cfde50890de0a3fd204c7020e
Hiroki Hayashi <yuriyuri.9129@gmail.com>
2024-06-20 14:26:03 +0900

    型を使用したprops,state

--- react-stepup/06_react_typescript/src/App.tsx
+++ react-stepup/06_react_typescript/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {Todo} from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 
 function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+          setTodos(response.data);
+        }
+      )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickFetchData}>データを取得</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
+      ))}
     </div>
   );
 }
---
+++ react-stepup/06_react_typescript/src/Todo.tsx
@@ -0,0 +1,11 @@
+type TodoType = {
+  userId: number;
+  title: string;
+  completed?: boolean;
+}
+
+export const Todo = (props: TodoType) => {
+  const {title, userId, completed = false} = props;
+  const completeMark = completed ? "[完]" : "[未]"
+  return <p>{`${completeMark}${title}(ユーザー:${userId})`}</p>
+}
