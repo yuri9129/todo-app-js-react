ebd6945b4b9f3f6d6a096abf6a31cd580777ba0a
Hiroki Hayashi <yuriyuri.9129@gmail.com>
2024-06-21 12:39:49 +0900

    ユーザー一覧取得と表示を実装

--- react-stepup/07_customhook/src/App.tsx
+++ react-stepup/07_customhook/src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {UserCard} from "./components/UserCard";
+import axios from "axios";
+import {User} from "./types/api/user";
+import {UserProfile} from "./types/UserProfile";
+
+const user = {
+  id: 1,
+  name: "John Doe",
+  email: "johndoe@gmail.com",
+  address: "my-address"
+}
+
 
 function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => {
+          return {
+            id: user.id,
+            name: `${user.name}(${user.username})`,
+            email: user.email,
+            address: `${user.address.city}${user.address.suite}${user.address.street}`,
+          }
+        });
+        setUserProfiles(data);
+      })
+      .catch(err => {
+        console.log(err)
+        setError(true);
+      })
+      .finally(() => setLoading(false));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br/>
+      {error ? (
+        <p style={{color: "red"}}>データの取得に失敗しました({error})</p>
+      ) : loading ? (
+        <p>Loading....</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => {
+            return <UserCard key={user.id} user={user}/>
+          })}
+        </>
+      )}
     </div>
   );
 }
